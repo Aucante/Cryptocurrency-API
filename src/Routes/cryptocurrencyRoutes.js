7630dbde1066711d0e028b23c609fa423dfef268
Aucante <sebastien.aucante@gmail.com>
2023-10-22 00:58:14 +0200

    Validation rules

--- src/Routes/cryptocurrencyRoutes.js
+++ src/Routes/cryptocurrencyRoutes.js
@@ -33,7 +33,15 @@ module.exports = (app) => {
             const message = 'Cryptocurrency created successfully.';
             res.json({ message, data: newCrypto });
         } catch (error) {
-            res.status(500).json({ error: 'Internal server error.' });
+            if (error.name === 'SequelizeUniqueConstraintError') {
+                res.status(400).json({ error: 'Cryptocurrency with the same code or name already exists.' });
+            } else if (error.name === 'SequelizeValidationError') {
+                const validationErrors = error.errors.map(err => err.message);
+                res.status(400).json({ error: 'Validation failed.', validationErrors });
+            } else {
+                console.error('Error:', error.name);
+                res.status(500).json({ error: 'Internal server error.' });
+            }
         }
     });
 
--- src/models/cryptocurrency.js
+++ src/models/cryptocurrency.js
@@ -3,15 +3,35 @@ module.exports = (sequelize, DataTypes) => {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
-            autoIncrement: true
+            autoIncrement: true,
         },
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
+            validate: {
+                notNull: {
+                    msg: 'The name is a required property.'
+                },
+                len: {
+                    args: [1, 15],
+                    msg: 'The name must be between 1 and 15 characters.'
+                }
+            }
         },
         code: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
+            validate: {
+                notNull: {
+                    msg: 'The code is a required property.'
+                },
+                len: {
+                    args: [3, 3],
+                    msg: 'The code must be exactly 3 characters long.'
+                }
+            }
         },
         picture: {
             type: DataTypes.STRING,
